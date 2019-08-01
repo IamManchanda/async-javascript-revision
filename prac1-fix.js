1f048558bf48f6f72fae90d02e6f17e1f75b0880
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-08-01 20:59:01 +0530

    Callback: Fixes printnum4 to execute after file read by printnum3 fn!

---
+++ prac1-fix.js
@@ -0,0 +1,19 @@
+const printNum1 = () => console.log(1);
+const printNum2 = () => {
+  const getNumber = (v) => v;
+  console.log(getNumber(2));
+};
+const printNum3 = () => {
+  const fs = require("fs");
+  fs.readFile("./num3.txt", "utf-8", function executeReadingFile(error, data) {
+    if (!error) {
+      console.log(Number(data));
+      printNum4();
+    }
+  });
+};
+const printNum4 = () => console.log(4);
+
+printNum1();
+printNum2();
+printNum3();
