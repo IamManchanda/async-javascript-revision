ad1ba0e4c34b8e6d61edc53c9767abd7303a049c
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-08-02 21:35:23 +0530

    Working pass on testing in promises!

---
+++ promises/testing/src/calculate-square.js
@@ -0,0 +1,13 @@
+const calculateSquare = (num) => {
+  return new Promise(function calculateSquarePromised(resolve, reject) {
+    setTimeout(function calculateSquareWithTimer() {
+      if (typeof num !== "number") {
+        return reject(new Error("Argument of type Number is expected."));
+      }
+      const result = num ** 2;
+      resolve(result);
+    }, 1000);
+  });
+};
+
+module.exports = calculateSquare;
---
+++ promises/testing/test/calculate-square.test.js
@@ -0,0 +1,29 @@
+const calculateSquare = require("../src/calculate-square.js");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
+describe("calculateSquare", function describeCalculateSquare() {
+  it(
+    "should resolve 4 when passed 2", 
+    function itShouldResolve4WhenPassed2(done) {
+      expect(calculateSquare(2)).to.eventually.be.equal(4).notify(done);
+    },
+  );
+
+  it(
+    "should fulfill when passed 2", 
+    function itShouldFulfillWhenPassed2(done) {
+      expect(calculateSquare(2)).to.be.fulfilled.notify(done);
+    },
+  );
+
+  it(
+    "should reject with an error when passed string", 
+    function itShouldRejectErrorWhenPassedString(done) {
+      expect(calculateSquare("string")).to.be.rejected.notify(done);
+    },
+  );
+});
