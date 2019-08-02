6088d51cbb19f9cb1eac0ffec94cab740933b9e1
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-08-02 22:53:06 +0530

    Handling multiple assertion tests!

--- promises/testing/test/calculate-square.test.js
+++ promises/testing/test/calculate-square.test.js
@@ -10,22 +10,31 @@ describe("calculateSquare", function describeCalculateSquare() {
 
   it(
     "should resolve 4 when passed 2", 
-    function itShouldResolve4WhenPassed2(done) {
-      expect(calculateSquare(2)).to.eventually.be.equal(4).notify(done);
+    function itShouldResolve4WhenPassed2() {
+      return calculateSquare(2).then(function handleMultipleAssertions(result) {
+        expect(result).to.be.above(3);
+        expect(result).to.be.equal(4);
+      });
     },
   );
 
   it(
     "should fulfill when passed 2", 
-    function itShouldFulfillWhenPassed2(done) {
-      expect(calculateSquare(2)).to.be.fulfilled.notify(done);
+    function itShouldFulfillWhenPassed2() {
+      return expect(calculateSquare(2)).to.be.fulfilled;
     },
   );
 
   it(
     "should reject with an error when passed string", 
-    function itShouldRejectErrorWhenPassedString(done) {
-      expect(calculateSquare("string")).to.be.rejected.notify(done);
+    function itShouldRejectErrorWhenPassedString() {
+      return calculateSquare("string")
+        .catch(
+          function handleRejectionError(error) {
+            expect(error).to.not.equal(null);
+            expect(error.message).to.equal("Argument of type Number is expected.");
+          },
+        );
     },
   );
 });
