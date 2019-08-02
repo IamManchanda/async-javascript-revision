202a97874c4bdec0d3ff0aa862f37d9eefaba7ca
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-08-02 21:35:05 +0530

    Code formatting - chore!

--- callbacks/testing/test/calculate-square.test.js
+++ callbacks/testing/test/calculate-square.test.js
@@ -2,18 +2,24 @@ const calculateSquare = require("../src/calculate-square.js");
 const expect = require("chai").expect;
 
 describe("calculateSquare", function describeCalculateSquare() {
-  it("should return 4 if passed 2", function itShouldReturn4IfPassed2(done) {
-    calculateSquare(2, function callbackShouldReturn4IfPassed2(error, result) {
-      expect(result).to.equal(4);
-      done();
-    });
-  });
+  it(
+    "should return 4 when passed 2", 
+    function itShouldReturn4WhenPassed2(done) {
+      calculateSquare(2, function callbackShouldReturn4WhenPassed2(error, result) {
+        expect(result).to.equal(4);
+        done();
+      });
+    },
+  );
 
-  it("should return an error if passed string", function itShouldReturnErrorIfPassedString(done) {
-    calculateSquare("string", function callbackShouldReturnErrorIfPassedString(error, result) {
-      expect(error).to.not.equal(null);
-      expect(error.message).to.equal("Argument of type Number is expected.");
-      done();
-    });
-  });
+  it(
+    "should return an error when passed string", 
+    function itShouldReturnErrorWhenPassedString(done) {
+      calculateSquare("string", function callbackShouldReturnErrorWhenPassedString(error, result) {
+        expect(error).to.not.equal(null);
+        expect(error.message).to.equal("Argument of type Number is expected.");
+        done();
+      });
+    },
+  );
 });
